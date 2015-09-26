aee828f37ffecfa0b497843dbbd9c1692956909c
Smirnoff <vlad4@umbc.edu>
2015-09-26 23:42:25 +0500

    Problem 1

---
+++ 1-problem.swift
@@ -0,0 +1,11 @@
+func p1() -> Int {
+  
+    return reduce(3...999, 0, { (sum: Int, value: Int) -> Int in
+        if value % 3 == 0 || value % 5 == 0 {
+            return sum + value
+        }
+        else {
+            return sum
+        }
+    })
+}
